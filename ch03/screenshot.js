a153b8e1b519c8fd4afd2c5ab7a0d8b740fe6225
임성현 <sunghyun.lim@gmail.com>
2017-08-13 00:38:34 +0900

    casper learning

---
+++ ch03/screenshot.js
@@ -0,0 +1,11 @@
+var casper = require('casper').create();
+
+casper.start();
+
+casper.open("http://jpub.tistory.com");
+
+casper.then(function(){
+	casper.capture("screenshot.png");
+});
+
+casper.run();
